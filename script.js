bdd71482ac984049375ec193f722f5e5994418e3
Greg Schuman <gcschuman@hotmail.com>
2021-01-16 11:18:48 -0800

    Added the script.

---
+++ script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('counter')
+
+counters.forEach(counter => {
+    counter.inntText = '0'
+
+    const updateCpunter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 200
+
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else{
+            counter.innerText = target
+        }
+    }
+
+    updateCpunter()
+})
